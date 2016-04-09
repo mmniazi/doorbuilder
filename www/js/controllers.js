8f8623503f549762aa6cc8c1da12bdbd5da50017
muhammadmohsin1994 <muhammad.mohsin1994@gmail.com>
2016-04-09 15:08:20 +0500

    removed sidebar

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,35 +1,19 @@
 angular.module('app.controllers', [])
 
-.controller('sidemenuCtrl', function($scope, $window) {
-	$scope.page = 1;
-
-	if ($window.innerWidth >= 768) {
-		$scope.lSideMenuWidth = $window.innerWidth * 0.2;
-		$scope.rSideMenuWidth = $window.innerWidth * 0.3;
-	} else {
-		$scope.lSideMenuWidth = $window.innerWidth * 0.6;
-		$scope.rSideMenuWidth = $window.innerWidth * 0.7;
-	}
-
-	$scope.setPage = function(page) {
-		$scope.page = page;
-	}
-
-	$scope.getMenuClass = function(page) {
-		return ($scope.page == page) ? "selectedMenu" : "deSelectedMenu";
-	}
+.controller('sidemenuCtrl', function ($scope, $window) {
+	$scope.rSideMenuWidth =
+		($window.innerWidth >= 768) $window.innerWidth * 0.3: $scope.rSideMenuWidth = $window.innerWidth * 0.7;
 })
 
-.controller('page1Ctrl', function($scope, doorService) {
+.controller('page1Ctrl', function ($scope, doorService) {
 	$scope.laminated = doorService.laminated;
-
-	$scope.setType = function(type) {
+	$scope.setType = function (type) {
 		$scope.laminated = type;
 		doorService.laminated = type;
 	}
 })
 
-.controller('page2Ctrl', function($scope, doorService, textures) {
+.controller('page2Ctrl', function ($scope, doorService, textures) {
 	$scope.texs = textures;
 	$scope.texture = doorService.texture;
 	$scope.setTex = function (tex) {
@@ -38,6 +22,4 @@ angular.module('app.controllers', [])
 	}
 })
 
-.controller('page3Ctrl', function($scope) {
-
-})
+.controller('page3Ctrl', function ($scope) {})
