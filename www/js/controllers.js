c5f5abf467fb8c7d16d1843bf3b5417379d841b1
muhammadmohsin1994 <muhammad.mohsin1994@gmail.com>
2016-04-13 10:43:07 +0500

    fudge

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,32 +1,49 @@
 angular.module('app.controllers', [])
-// TODO: add current price to each page
-// TODO: create front page loader
-// TODO: create a single controller for all pages
+	// TODO: add current price to each page
+	// TODO: create front page loader
+	// TODO: ask for aspect ratio of doors and images ratio should match it
+
 .controller('sidemenuCtrl', function ($scope, $window) {
 	$scope.sideMenuWidth =
 		($window.innerWidth >= 768) ? $window.innerWidth * 0.5 : $window.innerWidth * 0.7;
 })
 
-.controller('startCtrl', function ($scope, doorService) {
-	$scope.id = 1;
-	$scope.isLaminated = doorService.isLaminated;
+.controller('startCtrl', function ($scope, state) {
+	$scope.isLaminated = false;
 	$scope.set = function (type) {
 		$scope.isLaminated = type;
-		doorService.isLaminated = type;
+		state.set(1, type);
 	}
 })
 
-.controller('selectorCtrl', function ($scope, doorService, textures, $routeParams) {
-	$scope.id = $routeParams.id;
-	$scope.selected = doorService.texture;
-	$scope.images = textures;
-	$scope.set = function (texture) {
-		$scope.selected = texture;
-		doorService.texture = texture;
+.controller('selectorCtrl', function ($scope, state, images, $stateParams, $location, price) {
+	var id = $stateParams.id;
+	$scope.selected = state.get(id).value;
+	$scope.heading = state.get(id).name;
+	$scope.images = images(id);
+
+	if (!(id > 1 && id < 7)) {
+		$location.path("#/sidemenu/start/");
 	}
-})
 
-.controller('page7Ctrl', function ($scope, doorService, handles) {
-	$scope.id = 7;
+	$scope.backUrl = function () {
+		if (id == 4) {
+			return '#/sidemenu/start/';
+		} else if (id == 5) {
+			return (state.get("1").value) ? '#/sidemenu/selector/4' : '#/sidemenu/selector/3';
+		} else if (id == 2) {
+			return '#/sidemenu/start/';
+		} else {
+			return '#/sidemenu/selector/' + (id - 1);
+		}
+	}
 
+	$scope.nextUrl = function () {
+		return '#/sidemenu/selector/' + ((id == 3) ? 5 : parseInt(id) + 1);
+	}
+
+	$scope.set = function (selected) {
+		$scope.selected = selected;
+		state.set(id, selected);
+	}
 })
--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,156 @@
 angular.module('app.services', [])
 
-.service('doorService', function() {
-	this.isLaminated = true;
-	this.texture = 1;
-	this.lamination = 1;
-	this.design = 1;
-	this.hinge = 1;
-	this.handle = 1;
+.factory('state', function () {
+	var _isLaminated = {
+		name: "isLaminated",
+		value: false
+	};
+	var _textures = {
+		name: "Texture",
+		value: 1
+	};
+	var _designs = {
+		name: "Design",
+		value: 1
+	};
+	var _lamination = {
+		name: "Lamination",
+		value: 1
+	};
+	var _handles = {
+		name: "Handle",
+		value: 1
+	};
+	var _hinges = {
+		name: "Hinge",
+		value: 1
+	};
+
+	this.set = function (id, selected) {
+		switch (id) {
+			case "1":
+				_isLaminated.value = selected;
+				break;
+			case "2":
+				_textures.value = selected;
+				break;
+			case "3":
+				_designs.value = selected;
+				break;
+			case "4":
+				_lamination.value = selected;
+				break;
+			case "5":
+				_handles.value = selected;
+				break;
+			case "6":
+				_hinges.value = selected;
+				break;
+		}
+	}
+
+	this.get = function (id) {
+		switch (id) {
+			case "1":
+				return _isLaminated;
+			case "2":
+				return _textures;
+			case "3":
+				return _designs;
+			case "4":
+				return _lamination;
+			case "5":
+				return _handles;
+			case "6":
+				return _hinges;
+		}
+	}
+
+	return this;
 })
 
 // TODO: http://ionicframework.com/docs/v2/native/Base64%20To%20Gallery/
 
-.factory("textures", function($firebaseArray) {
-  var fire = new Firebase("https://doorsbuilder.firebaseio.com/textures");
-  return $firebaseArray(fire);
+.factory("images", function ($firebaseArray) {
+	var _textures = $firebaseArray(new Firebase("https://doorsbuilder.firebaseio.com/textures"));
+	var _lamination = $firebaseArray(new Firebase("https://doorsbuilder.firebaseio.com/lamination"));
+	var _hinges = $firebaseArray(new Firebase("https://doorsbuilder.firebaseio.com/hinges"));
+	var _handles = $firebaseArray(new Firebase("https://doorsbuilder.firebaseio.com/handles"));
+	var _designs = $firebaseArray(new Firebase("https://doorsbuilder.firebaseio.com/designs"));
+
+	return function (id) {
+		switch (id) {
+			case "2":
+				return _textures;
+			case "3":
+				return _designs;
+			case "4":
+				return _lamination;
+			case "5":
+				return _handles;
+			case "6":
+				return _hinges;
+		}
+	};
 })
 
-.factory("lamination", function($firebaseArray) {
-  var fire = new Firebase("https://doorsbuilder.firebaseio.com/lamination");
-  return $firebaseArray(fire);
-})
+.factory("price", function () {
+	var _textures = 0;
+	var _designs = 0;
+	var _lamination = 0;
+	var _handles = 0;
+	var _hinges = 0;
+	var _labour = 500;
+	var _total = 0;
 
-.factory("hinges", function($firebaseArray) {
-  var fire = new Firebase("https://doorsbuilder.firebaseio.com/hinges");
-  return $firebaseArray(fire);
-})
+	this.sum = function (id) {
+		switch (id) {
+			case "2":
+				return _textures;
+			case "3":
+				return _textures + _designs;
+			case "4":
+				return _textures + _designs + _lamination;
+			case "5":
+				return _textures + _designs + _lamination + _handles;
+			case "6":
+				return _textures + _designs + _lamination + _handles + _hinges;
+			case "7":
+				return _textures + _designs + _lamination + _handles + _hinges + _labour;
+		}
+	}
 
-.factory("handles", function($firebaseArray) {
-  var fire = new Firebase("https://doorsbuilder.firebaseio.com/handles");
-  return $firebaseArray(fire);
-})
+	this.get = function (id) {
+		switch (id) {
+			case "2":
+				return _textures;
+			case "3":
+				return _designs;
+			case "4":
+				return _lamination;
+			case "5":
+				return _handles;
+			case "6":
+				return _hinges;
+			case "7":
+				return _labour;
+		}
+	}
 
-.factory("frames", function($firebaseArray) {
-  var fire = new Firebase("https://doorsbuilder.firebaseio.com/frames");
-  return $firebaseArray(fire);
-})
+	this.set = function (id, price) {
+		switch (id) {
+			case "2":
+				_textures = price;
+			case "3":
+				_designs = price;
+			case "4":
+				_lamination = price;
+			case "5":
+				_handles = price;
+			case "6":
+				_hinges = price;
+		}
+	}
 
-.factory("designs", function($firebaseArray) {
-  var fire = new Firebase("https://doorsbuilder.firebaseio.com/designs");
-  return $firebaseArray(fire);
-})
-
-.factory("colors", function($firebaseArray) {
-  var fire = new Firebase("https://doorsbuilder.firebaseio.com/colors");
-  return $firebaseArray(fire);
+	return this;
 })
